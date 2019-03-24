396d7dee37b128ca486d08c2476843cfc8f8e1f8
Bruno Calmels <bruno.calmels@macherit.com>
2019-03-24 15:36:05 -0300

    feat[style]: Integra bootstrap, corrige URL de endpoint JSON, incluye mensajes de respuesta.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,26 +1,37 @@
 class HomeController < ApplicationController
   def index
+    if params[:dia].to_f > 10000.0
+      redirect_to root_path, alert: "¿Para qué quiere saber algo tan lejos en el futuro? Relájese y disfrute de la vida." and return
+    end
+    @params_in = params
+    @dia = params[:dia].to_i || 0
     @galaxia = GalaxiaService.new
+    @prediccion = @galaxia.prediccion(@dia)
     colineales = @galaxia.colineales
     @sequias = colineales[:sequias]
     @optimas = colineales[:optimas]
     triangular = @galaxia.triangular
     @lluvias = triangular[:lluvias]
     @perimetro = triangular[:perimetro]
+    @clima = @prediccion[:clima]
+    @posiciones = @prediccion[:posiciones]
     render 'home/index'
   end
 
-  def dia
-    @galaxia = GalaxiaService.new
-    @dia = params[:dia]
-    @prediccion = @galaxia.prediccion(@dia.to_f)
-    @clima = @prediccion[:clima]
-    @posiciones = @prediccion[:posiciones]
+  # def dia
 
-    index
-  end 
+  #   index
+  # end 
 
   def show_json
+    if params[:dia].to_f > 10000.0
+      render json: {
+        status: 403,
+        message: "¿Para qué quiere saber algo tan lejos en el futuro? Relájese y disfrute de la vida."
+      } and return
+    end
+    @params_in = params
+    @dia = params[:dia].to_i || 0
     @galaxia = GalaxiaService.new
     @dia = params[:dia]
     @prediccion = @galaxia.prediccion(@dia.to_f)
@@ -31,7 +42,8 @@ class HomeController < ApplicationController
         'Ferengi': @prediccion[:posiciones][:ferengi],
         'Vulcano': @prediccion[:posiciones][:vulcano],
         'Betasoide': @prediccion[:posiciones][:betasoide]
-      }
+      },
+      status: 200,
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/clima/dia=:dia', to: 'home#show_json'
-  get '/dia/:dia', to: 'home#dia'
+  get '/clima', to: 'home#show_json'
+  get '/:dia', to: 'home#index'
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
