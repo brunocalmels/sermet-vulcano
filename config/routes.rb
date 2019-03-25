8a7f19e1cc20a60263d79a899b1c63f41e8016b9
Bruno Calmels <bruno.calmels@macherit.com>
2019-03-25 01:03:57 -0300

    style: Un poco de estilado malo del sitio

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-
   get '/clima', to: 'home#show_json'
   get '/:dia', to: 'home#index'
 
   root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
